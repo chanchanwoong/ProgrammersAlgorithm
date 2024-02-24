854438aafe80fc1b4ceea9849009b2847c2bfec8
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-02-24 19:05:45 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT a.animal_id, a.name
+    from animal_ins a
+        inner join animal_outs b
+        on a.animal_id = b.animal_id
+    order by b.datetime - a.datetime desc
+    limit 2
+    ;
