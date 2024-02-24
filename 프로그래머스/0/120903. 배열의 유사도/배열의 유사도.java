f9e52a1dee6fd4317bf6db06e4a4e02f96740635
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-02-24 19:00:46 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        
+        for (String str1 : s1){
+            for (String str2 : s2){
+                answer += str1.equals(str2) ? 1 : 0;   
+            }
+        }
+        return answer;
+    }
+}
