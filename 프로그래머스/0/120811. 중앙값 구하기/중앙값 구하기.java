96f7edd8d23ef38e7c071d0dd8fc70a0ec16c5a6
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-02-24 18:13:24 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.45 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int length = array.length;
+        Arrays.sort(array);
+        answer = array[length/2];
+        return answer;
+    }
+}
