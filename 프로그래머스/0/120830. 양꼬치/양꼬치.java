5dc5eb472171c5c76edc3a62cc29eca9e9cf0c75
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-02-24 19:01:26 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int i = n >= 10 ? n / 10 : 0;
+        return answer = n * 12000 + (k - i) * 2000;
+    }
+}
