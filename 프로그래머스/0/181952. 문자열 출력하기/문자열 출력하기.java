945f939beef6ffe7ac0fcb4eec2791859a2346c6
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-02-24 19:14:13 +0900

    [level 0] Title: 문자열 출력하기, Time: 245.18 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Solution {
+
+	//	문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String output = sc.nextLine();
+		
+		System.out.println(output);
+	}
+}
