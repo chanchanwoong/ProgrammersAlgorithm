9378747fad541015b391d1c6279fd6a0813dd300
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-03-05 21:57:19 +0900

    [level 2] Title: 택배상자, Time: 197.63 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -6,11 +6,12 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for(int i=1; i<order.length+1; i++){
-            if(order[answer] == i){
-                answer++;
-            }else{
-                stack.push(i);
-            }
+            // if(order[answer] == i){
+            //     answer++;
+            // }else{
+            //     stack.push(i);
+            // }
+            stack.push(i);
             while(!stack.isEmpty() && order[answer] == stack.peek()){
                 answer++;
                 stack.pop();
