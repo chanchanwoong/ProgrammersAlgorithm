fb38a44db8357d552a005620b819142a276a6c8f
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-05-06 21:39:00 +0900

    [Silver III] Title: 01타일, Time: 136 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -2,17 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    public static int solution(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        // 초기 값
+        int data1 = 1;
+        int data2 = 2;
+        int sum = 0;
+        
+        for (int i = 2; i< n; i++) {
+            sum = (data1 + data2) % 15746;
+            data1 = data2;
+            data2 = sum;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[1000001];
-        dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
-        }
-
-        System.out.println(dp[n]);
+        System.out.println(solution(n));
     }
 }
