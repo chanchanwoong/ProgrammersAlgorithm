25407851b5161dade2bdece87067bf67aa8aef48
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-11-17 20:26:53 +0900

    [level 1] Title: 모의고사, Time: 1.86 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
 
 ### 성능 요약
 
-메모리: 83.6 MB, 시간: 1.34 ms
+메모리: 76.9 MB, 시간: 1.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 24일 19:14:42
+2024년 11월 17일 20:26:49
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,32 +1,27 @@
 import java.util.*;
+
 class Solution {
-    	static int[] answer1 = { 1, 2, 3, 4, 5 }; // 5개 반복
-	static int[] answer2 = { 2, 1, 2, 3, 2, 4, 2, 5 }; // 8개 반복
-	static int[] answer3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 }; // 10개 반복
-	static int[] scores = { 0, 0, 0 };
 
-    	public static List<Integer> solution(int[] answers) {
-		for (int i = 0; i < answers.length; i++) {
-			if (answers[i] == answer1[i % 5])
-				scores[0]++;
-			if (answers[i] == answer2[i % 8])
-				scores[1]++;
-			if (answers[i] == answer3[i % 10])
-				scores[2]++;
-		}
-
-		// 최댓값 점수 구하기
-		int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
-
-		// 리스트 객체 만들기
-		List<Integer> list = new ArrayList<>();
-
-		// 리스트 많이 맞춘 사람 넣기
-		for (int i = 0; i < 3; i++) {
-			if (max == scores[i]) {
-				list.add(i + 1);
-			}
-		}
-		return list;
+    public static List<Integer> solution(int[] answers) {
+        int[] pattern1 = {1, 2, 3, 4, 5};
+        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] scores = new int[3];
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < answers.length; i++) {
+            int answer = answers[i];
+            if (pattern1[i % pattern1.length] == answer) scores[0]++;
+            if (pattern2[i % pattern2.length] == answer) scores[1]++;
+            if (pattern3[i % pattern3.length] == answer) scores[2]++;
+        }
+        
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == maxScore) list.add(i+1);
+        }
+        
+    return list;
     }
 }
