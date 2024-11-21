6743366a3d7c1a46e25c04a88af3d0fc02085266
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-11-22 00:51:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.61 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,28 +1,21 @@
 import java.util.*;
 
 class Solution {
+
     public String solution(String[] participant, String[] completion) {
 		HashMap<String, Integer> map = new HashMap();
-
-		// HashMap에 participant마다 1씩 넣어주기
-		for (String str : participant) {
-			map.put(str, map.getOrDefault(str, 0) + 1);
-		}
-
-
-		// HashMap에서 completion 값 빼기
-		for (String str : completion) {
-			map.put(str, map.get(str) - 1);
-		}
-
-		String answer = "";
-		for (String str : map.keySet()) {
-			if(map.get(str) == 1){
-                answer = str;
-                break;
+        
+        for (String name : completion) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        for (String name : participant) {
+            if (map.getOrDefault(name, 0) == 0) {
+                return name;
             }
-		}
-		return answer;
-
+            map.put(name, map.get(name) - 1);
+        }
+        
+        return null;
     }
 }
