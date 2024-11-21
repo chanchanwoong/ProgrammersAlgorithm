a048fa661f0bd4f6aeb6f795bf1ba0b231beb931
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-11-22 00:02:02 +0900

    [level 1] Title: 카드 뭉치, Time: 0.25 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -3,12 +3,9 @@ import java.util.*;
 class Solution {
 
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        Queue<String> queue1 = new LinkedList<>();
-        Queue<String> queue2 = new LinkedList<>();
-        
-        for (String str : cards1) queue1.offer(str);
-        for (String str : cards2) queue2.offer(str);
-        
+        Queue<String> queue1 = new LinkedList<>(Arrays.asList(cards1));
+        Queue<String> queue2 = new LinkedList<>(Arrays.asList(cards2));
+
         for (String str : goal) {
             if (str.equals(queue1.peek())) queue1.poll();
             else if (str.equals(queue2.peek())) queue2.poll();
