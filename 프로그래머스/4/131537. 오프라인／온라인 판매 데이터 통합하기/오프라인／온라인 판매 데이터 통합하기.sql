d381e7f2799fe6b1be5ac4dff3f1c9fe753b7441
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-11-23 20:56:46 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,11 +1,17 @@
--- 코드를 입력하세요
-    SELECT date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, user_id, sales_amount
-        from online_sale
-        where date_format(sales_date, '%Y-%m') = '2022-03'    
-union
-    select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, null as user_id, sales_amount
-        from offline_sale
-        where date_format(sales_date, '%Y-%m') = '2022-03'
-    order by sales_date, product_id, user_id
-    
-    ;
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS 'SALES_DATE'
+    , PRODUCT_ID
+    , USER_ID
+    , SALES_AMOUNT
+FROM ONLINE_SALE
+WHERE DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03'
+UNION
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS 'SALES_DATE'
+    , PRODUCT_ID
+    , NULL AS 'USER_ID'
+    , SALES_AMOUNT
+FROM OFFLINE_SALE
+WHERE DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03' 
+ORDER BY SALES_DATE ASC
+    , PRODUCT_ID ASC
+    , USER_ID ASC
+;
