6eff6fc8497728b169569ce5d36492dae0a4c26b
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-11-23 21:01:19 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,6 @@
--- 코드를 입력하세요
-SELECT animal_id, name
-    from animal_ins
-    where intake_condition = 'sick'
-    order by animal_id;
+SELECT ANIMAL_ID
+    , NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID
+;
